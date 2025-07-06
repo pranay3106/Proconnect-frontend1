1e4138074e46eb30caac8324154536e850abeeda
Pranay Meshram <pranaymeshram0816@gmail.com>
2025-07-06 23:52:48 +0530

    updated styles

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -6,12 +6,19 @@ import { reset } from '@/config/redux/reducer/authReducer';
 
 export default function NavBar() {
 
+
+
+
   const router = useRouter();
 const authState = useSelector((state=>state.auth))
 const dispatch = useDispatch();
-const token= true
-//  = localStorage.getItem("token")
 
+
+
+
+  const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
+const isLoggedIn = authState.isLoggedIn || !!token;
+console.log(isLoggedIn)
   return (
     <>
     <div  className={styles.Container}>
@@ -21,10 +28,14 @@ const token= true
           ()=>{
             router.push("/")
           }
-        }>Pro Connect{console.log(authState.profilefetched)}
+        }>Pro Connect{console.log( "profilefetched feom nAVBAR",authState.profilefetched)}
         </h1>
+          {/* {console.log("autthhh",authState.isLoggedIn)} */}
+
+         
 <div className={styles.navbarOptions_div}>
  <div className={styles.navbarOptions}>
+
        {authState.profilefetched &&   authState.user && authState.user.userId && (
                         <div  className={styles.profile}>
                           <div style={{display:"flex",gap:"1.2rem",}} >
@@ -35,9 +46,8 @@ const token= true
                               }
                             }style={{fontWeight:"bold",cursor:"pointer",justifyContent:"center",alignItems:"center",padding:"1rem"}}>Profile</p>
                           </div>
-
-
-                           { token ?
+                        
+                           { isLoggedIn ?(
 
                         <div className={styles.buttonJoin}
                         onClick={()=>{
@@ -45,12 +55,20 @@ const token= true
                             dispatch(reset())
                             router.push("/login")
                       }}> <p>Log-out</p></div>
+                    )
+                    
+
                       :
-                      <div  onClick={()=>{
+                      (
+
+                        <div  onClick={()=>{
                           router.push("/login");
                         }}className={styles.buttonJoin}>
                           <p>Be a part</p>
                         </div>  
+                      )
+                      
+                     
                       
 
                         
--- src/config/index.jsx
+++ src/config/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const Base_Url = 
+export const Base_Url = "http://localhost:9091"
  "https://proconnectbakend.onrender.com/"
 
  export const clientServer = axios.create({
